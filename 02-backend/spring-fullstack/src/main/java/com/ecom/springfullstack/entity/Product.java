a928a2634173b68e1f48efae67b02b3fbfd24da8
Vignesh <vignesh000129@gmail.com>
2023-07-30 18:57:11 +0530

    Added Annotation and fields in the product file

--- 02-backend/spring-fullstack/src/main/java/com/ecom/springfullstack/entity/Product.java
+++ 02-backend/spring-fullstack/src/main/java/com/ecom/springfullstack/entity/Product.java
@@ -1,4 +1,28 @@
 package com.ecom.springfullstack.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
 public class Product {
+
+//    add fields
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unit_price;
+    private String image_url;
+    private boolean active;
+    private Long units_in_stock;
+    private Date date_created;
+    private Date last_updated;
+    private Long category_id;
+
 }
