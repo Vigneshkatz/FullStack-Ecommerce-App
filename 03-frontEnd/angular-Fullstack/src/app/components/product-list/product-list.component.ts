8a8923a894aba00fb260f9e0d573b3c2af21bb71
Vignesh <vignesh000129@gmail.com>
2023-07-31 23:03:37 +0530

    Update Angular component subscribe to data from Angular service

--- 03-frontEnd/angular-Fullstack/src/app/components/product-list/product-list.component.ts
+++ 03-frontEnd/angular-Fullstack/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  products : Product[] = [];
+  constructor(private productService : ProductService){
+
+  }
+  ngOnInit() : void{
+    this.listProducts();
+  }
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data =>{
+        this.products = data;
+      }
+    );
+  }
 }
